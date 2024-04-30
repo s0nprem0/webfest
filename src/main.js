6bb69024560e7bf416e786e2fed969eba2c23f94
son <s0nprem0@proton.me>
2024-05-01 03:45:51 +0800

    vite

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/util.css';
+
+
+// components
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+
