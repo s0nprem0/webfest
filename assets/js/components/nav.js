9206c18ecf36ce954b3636b623c067401eae166f
son <s0nprem0@proton.me>
2024-04-30 17:09:31 +0800

    yes

--- assets/js/components/nav.js
+++ assets/js/components/nav.js
@@ -1,18 +1,15 @@
 const $ = (e) => { return document.querySelector(e); }
 
 const navbar_component = `
-<nav class="wrapper header__navbar">
-        <div class="site-branding">
-            <img class="logo" width="64px" src="../assets/org/elits.svg" alt="ELITS">
-            </div>
-        </div>
+<nav class="header__navbar">
+        <img data-location="home" class="nav-link logo" src="/assets/org/elits.svg" alt="ELITS">
         <ul class="nav-menu">
-            <li><a class="nav-link" href="" data-location="">Home</a></li>
-            <li><a class="nav-link" href="" data-location="">About</a></li>
-            <li><a class="nav-link" href="" data-location="">Shop</a></li>
-            <li><a class="nav-link" href="" data-location="">about/dev</a></li>
+            <li><a class="nav-link" href="" data-location="home" >Home</a></li>
+            <li><a class="nav-link" href="" data-location="about" >About</a></li>
+            <li><a class="nav-link" href="" data-location="" >Shop</a></li>
+            <li><a class="nav-link" href="" data-location="dev">About/Dev</a></li>
         </ul>
     </nav>
-    `
-$('.navbar').innerHTML = navbar_component;
-$('.navbar').innerHTML = footer_component;
+`
+
+$('.navbar').outerHTML = navbar_component;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("/assets/js/routes.json")
+        .then((response) => response.json())
+        .then((routes) => {
+            const links = document.querySelectorAll(".nav-link");
+
+            links.forEach((link) => {
+                const route = link.getAttribute("data-location");
+                const path = routes[route] || routes[404];
+                link.setAttribute("href", path);
+            });
+
+            links.forEach((link) => {
+                link.addEventListener("click", async (event) => {
+                    event.preventDefault();
+
+                    try {
+                        const route = link.getAttribute("data-location");
+                        const target = link.getAttribute("target") || "_self";
+                        const path = await getPath(route);
+
+                        if (path) {
+                            window.open(path, target);
+                        } else {
+                            throw new Error("Route not found");
+                        }
+                    } catch (error) {
+                        window.open(routes[404], "_self");
+                        console.error("Error navigating:", error.message);
+                    }
+                });
+            });
+
+            async function getPath(route) {
+                return new Promise((resolve) => {
+                    setTimeout(() => {
+                        const path = routes[route] || null;
+                        resolve(path);
+                    }, 750);
+                });
+            }
+        })
+        .catch((error) => console.log("Error fetching data:", error));
+});
+
+
+
 // Assume that I want to replace all images on the page
 const images = document.querySelectorAll('img');
 
 // Loop over them
 [].forEach.call(images, function (ele) {
     ele.addEventListener('error', function (e) {
-        e.target.src = '/path/to/404/image.png';
+        e.target.src = '/assets/svg/ceit-sc.svg';
     });
 });
