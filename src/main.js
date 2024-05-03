7d21046958f58dfaa789c6f481a37bbf3515c06b
son <s0nprem0@proton.me>
2024-05-03 18:11:36 +0800

    Update HTML and CSS files for organization accomplishments section
    This commit updates the `index.html` file to rename the "Awards" section to "Organization Accomplishments" and adds a descriptive paragraph. The

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import '../styles/modern-normalize.css';
-import '../styles/style.css';
-import '../styles/util.css';
+    import '../styles/modern-normalize.css';
+    import '../styles/style.css';
+    import '../styles/util.css';
 
 
-// components
-import '../styles/components/header.css';
-import '../styles/components/hero.css';
-import '../styles/components/history.css';
-import '../styles/components/award.css';
-import '../styles/components/news.css'
-import '../styles/components/recruitment.css';
-import '../styles/components/footer.css';
+    // components
+    import '../styles/components/header.css';
+    import '../styles/components/hero.css';
+    import '../styles/components/history.css';
+    import '../styles/components/award.css';
+    import '../styles/components/news.css'
+    import '../styles/components/recruitment.css';
+    import '../styles/components/footer.css';
+
+    // about.html
+    import '../styles/components/about.css';
