04a7db5afa3a801f4bf5cc1c15033cbfa712b9cc
son <s0nprem0@proton.me>
2024-05-14 23:03:38 +0800

    refactor: footer cleanup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,81 @@
 import logo from "../assets/elits.svg";
+import {
+  RiMapPinFill,
+  RiFacebookCircleFill,
+  RiMailLine,
+  RiPhoneFill,
+  RiCalendarLine,
+  RiFileListLine,
+  RiAccountBoxLine,
+} from "react-icons/ri";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-8">
-      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 px-4 md:grid-cols-3 gap-4">
+        {/* Contact Us Section */}
         <div className="space-y-2">
-          <h4 className="font-bold">Contact US</h4>
-          <li>
-            <i className="ri-map-pin-fill text-white mr-2"></i>
-            <span>
-              Department of Information Technology, College of Engineering and
-              Information Technology, Cavite State University - Don Severino de
-              las Alas Campus
-            </span>
-          </li>
-          <li>
-            <a className="hyperlink" href="https://www.facebook.com/CvSUELITS">
-              <i className="ri-facebook-circle-fill text-white mr-2"></i>
-              <span>CVSU Elite League of Information Technology Students </span>
-            </a>
-          </li>
-          <li>
-            <a className="hyperlink">
-              <i className="ri-mail-line text-white mr-2"></i>
-              <span>elits.cvsu@gmail.com</span>
-            </a>
-          </li>
-          <li>
-            <a className="hyperlink">
-              <i className="ri-phone-fill text-white mr-2"></i>
-              <span>+63 912 345 6789</span>
-            </a>
-          </li>
+          <h4 className="font-bold">Contact Us</h4>
+          <ul className="list-none space-y-1">
+            <li className="flex items-center">
+              <RiMapPinFill className="h-6 w-6 mr-2 fill-current" />
+              <span>
+                Department of Information Technology, College of Engineering and
+                information technology
+              </span>
+            </li>
+            <li className="flex items-center">
+              <RiFacebookCircleFill className="mr-2 h-4 w-4" />
+              <a href="https://www.facebook.com/CvSUELITS">
+                CVSU Elite League of Information Technology Students
+              </a>
+            </li>
+            <li className="flex items-center">
+              <RiMailLine className="mr-2 h-4 w-4" />
+              <a href="mailto:elits.cvsu@gmail.com">elits.cvsu@gmail.com</a>
+            </li>
+            <li className="flex items-center">
+              <RiPhoneFill className="mr-2 h-4 w-4" />
+              <a href="tel:+639123456789">+63 912 345 6789</a>
+            </li>
+          </ul>
         </div>
+
+        {/* Quick Links Section */}
         <div className="space-y-2">
           <h4 className="font-bold">Quick Links</h4>
-          <li>
-            <i className="ri-file-list-line text-white mr-2"></i>
-            <a href="https://myportal.cvsu.edu.ph">Student Portal</a>
-          </li>
-          <li>
-            <i className="ri-account-box-line text-white mr-2"></i>
-            <a href="https://registrar.cvsu.edu.ph">
-              List of Enrolled Students
-            </a>
-          </li>
-          <li>
-            <i className="ri-calendar-line text-white mr-2"></i>
-            <a href="https://cvsu.edu.ph/university-calendar/">
-              University Calendar
-            </a>
-          </li>
+          <ul className="list-none space-y-1">
+            <li className="flex items-center">
+              <RiFileListLine className="mr-2" />
+              <a href="https://myportal.cvsu.edu.ph">Student Portal</a>
+            </li>
+            <li className="flex items-center">
+              <RiAccountBoxLine className="mr-2" />
+              <a href="https://registrar.cvsu.edu.ph">
+                List of Enrolled Students
+              </a>
+            </li>
+            <li className="flex items-center">
+              <RiCalendarLine className="mr-2" />
+              <a href="https://cvsu.edu.ph/university-calendar/">
+                University Calendar
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="space-y-2">
-          <li>Terms of Service</li>
-          <li>Privacy Policy</li>
-          <li>Credits</li>
-          <li>Copyright</li>
+
+        {/* Legal Section */}
+        <div>
+          <ul className="list-none space-y-2">
+            <li>Terms of Service</li>
+            <li>Privacy Policy</li>
+            <li>Credits</li>
+            <li>Copyright</li>
+          </ul>
         </div>
       </div>
-      <div className="container mx-auto py-4 text-center">
+
+      <div className="container mx-auto p-4 text-center">
         <p>
           &copy; {new Date().getFullYear()} Elite League of Information
           Technology Students. All rights reserved.
