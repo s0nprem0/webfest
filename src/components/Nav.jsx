8c2056bd5712fa9916fb7f033d1def1c7548db74
son <s0nprem0@proton.me>
2024-05-23 11:20:16 +0800

    fix: nav add link placeholder "About Dev"

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,10 @@ function Nav() {
           </li>
           <li>Shop</li>
           <li>
-            <a className="px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
+            <a
+              className="px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+              href="#"
+            >
               About dev
             </a>
           </li>
