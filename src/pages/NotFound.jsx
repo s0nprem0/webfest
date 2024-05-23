53692fec175321c315072a9733728e4602eab5bd
son <s0nprem0@proton.me>
2024-05-23 11:19:33 +0800

    feat: add 404

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+const NotFound = () => {
+  return (
+    <div className="container mx-auto">
+      <div className=" bg-blue-900 p-6">
+        <h1 className="font-bold">History</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolor,
+          totam fugit numquam harum placeat nobis alias tempore quia!
+          Perferendis quasi similique magni asperiores quo labore magnam enim
+          nulla harum.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
