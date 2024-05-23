aeec021c77b9deae80768fe8726c7e7a248711c6
son <s0nprem0@proton.me>
2024-05-23 11:31:52 +0800

    refactor: move header and footer to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 // import pages
 import Home from "./pages/Home.jsx";
@@ -18,6 +19,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Nav />
     <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
-import Nav from "../components/Nav";
 import Hero from "../components/Hero";
-import Footer from "../components/Footer";
 import History from "../components/History";
 
 function Home() {
   return (
     <>
-      <Nav />
       <Hero />
       <History />
-      <Footer />
     </>
   );
 }
